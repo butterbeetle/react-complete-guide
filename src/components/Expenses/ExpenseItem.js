fbf66c640137030ef5ce73213de3545b331598eb
butterbeetle <butterbeetle@naver.com>
2022-09-16 23:52:16 +0900

    Section 5: Rendering List

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -11,7 +11,7 @@ const ExpenseItem = (props) => {
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </Card>
-  )
-}
+  );
+};
 
-export default ExpenseItem; 
+export default ExpenseItem;
