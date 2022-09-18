31079856d1865b8868e5bd0baa66fc955d52e5db
butterbeetle <butterbeetle@naver.com>
2022-09-18 19:16:40 +0900

    Section 07. Prepare React App Debuging

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App01 from "./App01";
 import App02 from "./App02";
+import App03 from "./App03";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     {/* <App01 /> */}
-    <App02 />
+    {/* <App02 /> */}
+    <App03 />
   </div>
 );
