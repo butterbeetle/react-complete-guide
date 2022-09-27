2c833d2dbecc1bf3c035659e79b07ddeeca6f843
butterbeetle <butterbeetle@naver.com>
2022-09-28 00:03:23 +0900

    Section 13. 오류 경계

--- src/section13/components/Users.js
+++ src/section13/components/Users.js
@@ -11,6 +11,13 @@ class Users extends Component {
       more: "TEST",
     };
   }
+
+  componentDidUpdate() {
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
   toggleUsersHandler() {
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
