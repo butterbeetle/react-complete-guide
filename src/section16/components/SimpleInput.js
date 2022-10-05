77ea939497c966de367e51a9fa9aca62b3452841
butterbeetle <butterbeetle@naver.com>
2022-10-05 23:19:31 +0900

    Section16. 사용자 입력 값 가져오기

--- src/section16/components/SimpleInput.js
+++ src/section16/components/SimpleInput.js
@@ -1,9 +1,34 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameInputRef = useRef();
+  const [enteredName, setEnteredName] = useState("");
+
+  const nameInputChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    console.log(enteredName);
+    setEnteredName("");
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    // nameInputRef.current.value = ""; ==> 좋은 방법이 아니며 DOM 조작은 React로만 해야함.
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmissionHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={nameInputRef}
+          type="text"
+          id="name"
+          onChange={nameInputChangeHandler}
+          value={enteredName}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
