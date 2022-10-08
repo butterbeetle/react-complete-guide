e2ef9e757fce493fb21fc89e49ec09f302e084f1
butterbeetle <butterbeetle@naver.com>
2022-10-08 23:46:38 +0900

    Section18. 작업에 페이로드 연결

--- src/section18/components/Counter.js
+++ src/section18/components/Counter.js
@@ -9,6 +9,11 @@ const Counter = () => {
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
   };
+
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
   };
@@ -21,6 +26,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
