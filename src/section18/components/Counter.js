6c2f9aae18ddc93819cdeb24a6cb7bb0a0a0cbfb
butterbeetle <butterbeetle@naver.com>
2022-10-08 23:42:17 +0900

    Section18. 클래스 기반에서 리덕스 사용

--- src/section18/components/Counter.js
+++ src/section18/components/Counter.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import classes from "./Counter.module.css";
 
+// 컴포넌트 기반
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
@@ -26,5 +27,46 @@ const Counter = () => {
     </main>
   );
 };
-
 export default Counter;
+
+// 클래스 기반
+// import { Component } from "react";
+// import { connect } from "react-redux";
+// class Counter extends Component {
+//   incrementHandler() {
+//     this.props.increment();
+//   }
+//   decrementHandler() {
+//     this.props.decrement();
+//   }
+//   toggleCounterHandler() {}
+
+//   render() {
+//     return (
+//       <main className={classes.counter}>
+//         <h1>Redux Counter</h1>
+//         <div className={classes.value}>{this.props.counter}</div>
+//         <div>
+//           <button onClick={this.incrementHandler.bind(this)}>Increment</button>
+//           <button onClick={this.decrementHandler.bind(this)}>Decrement</button>
+//         </div>
+//         <button onClick={this.toggleCounterHandler}>Toggle Counter</button>
+//       </main>
+//     );
+//   }
+// }
+
+// const mapStateToProps = (state) => {
+//   return {
+//     counter: state.counter,
+//   };
+// };
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     increment: () => dispatch({ type: "increment" }),
+//     decrement: () => dispatch({ type: "decrement" }),
+//   };
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(Counter);
