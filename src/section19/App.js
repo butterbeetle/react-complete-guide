33cde39f9b4a97f7ee4a5fd8dbe4873cd57265e9
butterbeetle <butterbeetle@naver.com>
2022-10-11 01:33:11 +0900

    Section19. 데이터 가져오기

--- src/section19/App.js
+++ src/section19/App.js
@@ -4,8 +4,7 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import Notification from "./components/UI/Notification";
-import { sendCartData } from "./store/cart-slice";
-
+import { fetchCartData, sendCartData } from "./store/cart-actions";
 let isInitial = true;
 
 function App() {
@@ -13,6 +12,10 @@ function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
   const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
   useEffect(() => {
     if (isInitial) {
