f723377807404b910524e0e5e92f1dd0c2f2b4e3
butterbeetle <butterbeetle@naver.com>
2022-10-12 23:13:34 +0900

    Section20. 링크 작업하기

--- src/section20/App.js
+++ src/section20/App.js
@@ -1,16 +1,22 @@
 import { Route } from "react-router-dom";
+import MainHeader from "./components/MainHeader";
 import Produects from "./pages/Products";
 import Welcome from "./pages/Welcome";
 
 function App() {
   return (
     <div>
-      <Route path="/welcome">
-        <Welcome />
-      </Route>
-      <Route path="/products">
-        <Produects />
-      </Route>
+      <header>
+        <MainHeader />
+      </header>
+      <main>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/products">
+          <Produects />
+        </Route>
+      </main>
     </div>
   );
 }
