4633a7abf4becc8ec4c647237551fc1742b72827
butterbeetle <butterbeetle@naver.com>
2022-10-13 00:54:27 +0900

    Section20. 더미 데이터 및 추가 콘텐츠 추가하기

--- src/section20/pages/AllQuotes.js
+++ src/section20/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from "../components/quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "LEE",
+    text: "Learning React is great!",
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>AllQuotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
