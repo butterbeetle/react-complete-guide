1fd4267a8c7ce07dcde11cf4204301fc968b7c0b
butterbeetle <butterbeetle@naver.com>
2022-10-16 23:50:21 +0900

    Section22.자동 로그아웃 추가하기

--- src/section22/store/auth-context.js
+++ src/section22/store/auth-context.js
@@ -7,21 +7,34 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemoainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
   const initialToken = localStorage.getItem("token");
   const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token; // !! => 토큰이 빈 문자열이 아니면 True 비었으면 False
 
-  const loginHandler = (token) => {
-    setToken(token);
-    localStorage.setItem("token", token);
-  };
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem("token");
   };
 
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemoainingTime(expirationTime);
+
+    setTimeout(logoutHandler, remainingTime);
+  };
   const contextValue = {
     token: token,
     isLoggedIn: userIsLoggedIn,
