dc17cf3421dcf911ce9a3c9063400292f80bcc1e
butterbeetle <butterbeetle@naver.com>
2022-10-16 21:25:27 +0900

    Sesction22.사용자에게 피드백 표시하기

--- src/section22/components/Auth/AuthForm.js
+++ src/section22/components/Auth/AuthForm.js
@@ -5,7 +5,9 @@ import classes from "./AuthForm.module.css";
 const AuthForm = () => {
   const eamilInputRef = useRef();
   const passwordInputRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -17,6 +19,7 @@ const AuthForm = () => {
     const enteredEamil = eamilInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -33,10 +36,15 @@ const AuthForm = () => {
           },
         }
       ).then((res) => {
+        setIsLoading(false);
         if (res.ok) {
         } else {
           return res.json().then((data) => {
-            console.log(data);
+            let errorMessage = "Authentication failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
           });
         }
       });
@@ -61,7 +69,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
