3adb67ce3cba1099ca1024dae5ae5cad9226b3ba
butterbeetle <butterbeetle@naver.com>
2022-10-21 00:25:32 +0900

    Section28. useEffect & Cleanup

--- src/section28/components/Ingredients/Search.js
+++ src/section28/components/Ingredients/Search.js
@@ -9,7 +9,7 @@ const Search = React.memo((props) => {
   const inputRef = useRef();
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       if (enteredFilter === inputRef.current.value) {
         const query =
           enteredFilter.length === 0
@@ -35,6 +35,9 @@ const Search = React.memo((props) => {
           });
       }
     }, 500);
+    return () => {
+      clearTimeout(timer);
+    };
   }, [enteredFilter, onLoadIngredients, inputRef]);
 
   const ChangeHandler = (event) => {
