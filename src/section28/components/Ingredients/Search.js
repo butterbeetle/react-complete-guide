ed2278ab51b485342ecac5da5ff95fc38b304913
butterbeetle <butterbeetle@naver.com>
2022-10-21 00:22:15 +0900

    Section28. useRef

--- src/section28/components/Ingredients/Search.js
+++ src/section28/components/Ingredients/Search.js
@@ -1,37 +1,41 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
 
 const Search = React.memo((props) => {
   const [enteredFilter, setEnteredFilter] = useState("");
-
   const { onLoadIngredients } = props;
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(
-      "https://react-http-cc93a-default-rtdb.firebaseio.com/ingredients.json" +
-        query
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((responseData) => {
-        const loadedData = [];
-        for (const key in responseData) {
-          loadedData.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-http-cc93a-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => {
+            return response.json();
+          })
+          .then((responseData) => {
+            const loadedData = [];
+            for (const key in responseData) {
+              loadedData.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedData);
           });
-        }
-        onLoadIngredients(loadedData);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   const ChangeHandler = (event) => {
     event.preventDefault();
@@ -43,7 +47,12 @@ const Search = React.memo((props) => {
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" value={enteredFilter} onChange={ChangeHandler} />
+          <input
+            ref={inputRef}
+            type="text"
+            value={enteredFilter}
+            onChange={ChangeHandler}
+          />
         </div>
       </Card>
     </section>
