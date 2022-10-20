7bb71f82009f1c6a58d777ae0f2216935a635cd4
butterbeetle <butterbeetle@naver.com>
2022-10-21 00:09:59 +0900

    Section28. useEffect

--- src/section28/components/Ingredients/Ingredients.js
+++ src/section28/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,18 +7,57 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://react-http-cc93a-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  const filteredIngerientsHandler = (filteredIngredients) => {
+    setUserIngredients(filteredIngredients);
+  };
+
   const addIngredientHandler = (ingredient) => {
-    setUserIngredients((prevIngredient) => [
-      ...prevIngredient,
-      { id: Math.random().toString(), ...ingredient },
-    ]);
+    fetch(
+      "https://react-http-cc93a-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: {
+          "Content-Type": "applcation/json",
+        },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredient) => [
+          ...prevIngredient,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngerientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
--- src/section28/components/Ingredients/Search.js
+++ src/section28/components/Ingredients/Search.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
-import Card from '../UI/Card';
-import './Search.css';
+import Card from "../UI/Card";
+import "./Search.css";
 
-const Search = React.memo(props => {
+const Search = React.memo((props) => {
+  const [enteredFilter, setEnteredFilter] = useState("");
+
+  const { onLoadIngredients } = props;
+
+  useEffect(() => {
+    const query =
+      enteredFilter.length === 0
+        ? ""
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    fetch(
+      "https://react-http-cc93a-default-rtdb.firebaseio.com/ingredients.json" +
+        query
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const loadedData = [];
+        for (const key in responseData) {
+          loadedData.push({
+            id: key,
+            title: responseData.title,
+            amount: responseData.amount,
+          });
+        }
+        // onLoadIngredients(loadedData);
+      });
+  }, [enteredFilter, onLoadIngredients]);
+
+  const ChangeHandler = (event) => {
+    event.preventDefault();
+    setEnteredFilter(event.target.value);
+  };
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input type="text" value={enteredFilter} onChange={ChangeHandler} />
         </div>
       </Card>
     </section>
